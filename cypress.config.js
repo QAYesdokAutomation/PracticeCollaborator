318f895d558ac2ef7da85168772b4db5297ca123
putriewfauziahr <putrifauziahrahman@gmail.com>
2023-11-25 01:32:20 +0700

    Test cases Shopping Cart

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern : "cypress/support/E2E",
+    supportFile : false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    //cypress configuration
+    baseUrl : 'https://magento.softwaretestingboard.com/',
   },
 });
